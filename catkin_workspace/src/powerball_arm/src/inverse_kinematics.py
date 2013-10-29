480b88412d13d02d0365ff62dddb0bb7f7e16a22
Bryant <rcxking@yahoo.com>
2013-10-29 02:37:13 -0400

    Installed numpy for matrix support:

--- catkin_workspace/src/powerball_arm/src/inverse_kinematics.py
+++ catkin_workspace/src/powerball_arm/src/inverse_kinematics.py
@@ -2,14 +2,23 @@
 
 '''
 inverse_kinematics.py - rosnode to calculate the inverse kinematics for the
-						powerball arm.
+			powerball arm.
 
 Bryant Pong
 10/28/13
-Last Updated: 10/28/13 - 10:33 AM
+Last Updated: 10/29/13 - 2:11 AM
 '''
 
-import rospy
+# Import the rospy ROS Python library:
+#import rospy
+
+# Python Standard Libraries:
+from math import * # Access to pi, trig. functions
+
+# Import numpy for matrices and relevant operations:
+from numpy import *
+
+
 
 
 
