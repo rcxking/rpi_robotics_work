246c094f56e825d8c3e369249add1f098f171411
Powerball Lab <powerball>
2014-03-18 14:58:50 -0400

    Finished modifying powerball_script

--- powerball_software/powerball_script.sh
+++ powerball_software/powerball_script.sh
@@ -6,7 +6,7 @@
 # Bryant Pong
 # 2/24/14
 #
-# Last Updated: 2/28/14 - 1:08 PM
+# Last Updated: 2/28/14 - 3:13 PM
 
 # Start the SNS logging daemon and create sns-log
 sudo /usr/local/etc/init.d/sns start      
@@ -16,4 +16,4 @@ ach mk ref
 ach mk state  
 
 # Finally, start up the can402 CAN interface daemon:
-can402 -f can1 -a socketcan -n 7 -c ref -s state    
+can402 -f can1 -a socketcan -n 3 -c ref -s state    
