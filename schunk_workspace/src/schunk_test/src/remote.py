694eff1bbb3e7caad12b47062a67905ac3cce60b
Bryant Pong <rcxking@yahoo.com>
2014-10-01 15:31:17 -0400

    Added beginning of remote.py

---
+++ schunk_workspace/src/schunk_test/src/remote.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+'''
+remote.py - This program asks the user to input a series of target joint angles
+to the Powerball.  The Powerball will then generate a trajectory and move to 
+the appropriate task destination.
+
+Bryant Pong
+RPI CS Robotics Lab
+10/1/14
+
+Last Updated: 10/1/14 - 3:22 PM
+'''     
+
+# Standard Python Libraries:
+import time
+
+# ROS-Specific Libraries: 
+import rospy
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
